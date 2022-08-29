ed8b0daf76076b55ff18d73586b00ed7ee26f7d4
SenLongYu <94999339+SenLongYu@users.noreply.github.com>
2022-08-29 10:55:33 -0400

    Update environment.prod.ts

--- Cosminis/AngularCosminis/src/environments/environment.prod.ts
+++ Cosminis/AngularCosminis/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'https://cosminis.azurewebsites.net/'
+  api: 'https://cosminis.azurewebsites.net/',
   auth:
   {
     domain: "dev-lj8y5w8u.us.auth0.com",
