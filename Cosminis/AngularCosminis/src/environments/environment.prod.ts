c1907d1946c65e8409c65e72386787a76796e35a
SenLongYu <94999339+SenLongYu@users.noreply.github.com>
2022-08-29 10:49:04 -0400

    Update environment.prod.ts
    Auth0 shouldn't complaint now

--- Cosminis/AngularCosminis/src/environments/environment.prod.ts
+++ Cosminis/AngularCosminis/src/environments/environment.prod.ts
@@ -1,4 +1,10 @@
 export const environment = {
   production: true,
   api: 'https://cosminis.azurewebsites.net/'
+  auth:
+  {
+    domain: "dev-lj8y5w8u.us.auth0.com",
+    clientId: "tGqEiAcYxB9BZh4VpzFXgKbuI82Se4pk",
+    redirectUri: window.location.origin
+  }
 };
