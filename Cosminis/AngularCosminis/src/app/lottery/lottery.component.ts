5449b77eca27f4f22c46e3b5164ae40449e7d43f
krisholl <krisholl2022@gmail.com>
2022-08-30 15:00:59 -0400

    Problem

--- Cosminis/AngularCosminis/src/app/lottery/lottery.component.ts
+++ Cosminis/AngularCosminis/src/app/lottery/lottery.component.ts
@@ -3,15 +3,7 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
 @Component({
   selector: 'app-lottery',
   templateUrl: './lottery.component.html',
-  styleUrls: ['./lottery.component.css'],
-  animations: [
-    // Each unique animation requires its own trigger. The first argument of the trigger function is the name
-    trigger('rotatedState', [
-      state('default', style({ transform: 'rotate(0)' })),
-      state('rotated', style({ transform: 'rotate(-180deg)' })),
-      transition('rotated => default', animate('1500ms ease-out')),
-      transition('default => rotated', animate('400ms ease-in'))
-    ])]
+  styleUrls: ['./lottery.component.css'] 
 })
 export class LotteryComponent implements OnInit {
   startAngle: number = 0;
