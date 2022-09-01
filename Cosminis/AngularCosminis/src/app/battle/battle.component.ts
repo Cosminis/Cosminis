7c712669b351183ba42c6377a3a84fadd45fa17c
Elizabeth Kang <liz.kang@hey.com>
2022-09-01 11:30:43 -0500

    Added methods to the battle component for gameplay

--- Cosminis/AngularCosminis/src/app/battle/battle.component.ts
+++ Cosminis/AngularCosminis/src/app/battle/battle.component.ts
@@ -20,37 +20,69 @@ export class BattleComponent implements OnInit {
     private friend: FriendsService,
     private router: Router) { }
   
+  OpponentRoster: Cosminis[] = [];
+  PlayerRoster: Cosminis[] = [];
+  PlayerGoldBet: number = 0;
+  PlayerRisk: number = 0;
+
+  OpponentCosmini2Battle: Cosminis = {
+    companionId: 0,
+    trainerId : 0,
+    userFk : 0,
+    speciesFk : 0,
+    nickname : "OpponentCosmini",
+    emotion : 0,
+    mood : 0,
+    hunger : 0,
+  };
+
+  PlayerCosmini2Battle: Cosminis = {
+    companionId: 0,
+    trainerId : 0,
+    userFk : 0,
+    speciesFk : 0,
+    nickname : "PlayerCosmini",
+    emotion : 0,
+    mood : 0,
+    hunger : 0,
+  };
   
-  hi: string = 'hi';
-
-  GetRandomFriendsUserId(): number {
-    let stringUser: string = sessionStorage.getItem('currentUser') as string;
-    let currentUser: Users = JSON.parse(stringUser);
-    let FriendId: number = 0;
-    let friends: Friends[] = [];
-
-    this.friend.getAcceptedFriends(currentUser.username).subscribe((res) => friends = res);
-    
-    //gets a rando friends from user's friend list
-    let randoFriendinArr: number = Math.floor(Math.random() * friends.length);
-    if (friends[randoFriendinArr].userIdTo == currentUser.userId) {
-      FriendId = friends[randoFriendinArr].userIdFrom;
-    }
-    else {
-      FriendId = friends[randoFriendinArr].userIdTo;
-    }
-    return FriendId;
+  CreateARandoRoster() {
+    this.battle.CreateRoster().subscribe((res) => this.OpponentRoster = res);
   }
 
-  GetRandomId(): number {
-    let stringUser: string = sessionStorage.getItem('currentUser') as string;
-    let currentUser: Users = JSON.parse(stringUser);
-    let FriendId: number = 0;
-
-    return 0;
+  CreateAFriendRoster() {
+    this.battle.CreateRosterWithId(this.battle.GetRandomFriendsUserId()).subscribe((res) => this.OpponentRoster = res);
   }
 
-  
+  CreatePlayerRoster() {
+    let stringUser: string = sessionStorage.getItem('currentUser') as string;
+    let currentUser: Users = JSON.parse(stringUser);
+    this.battle.CreateRosterWithId(currentUser.userId as number).subscribe((res) => this.PlayerRoster = res);
+
+  }
+
+  CutRosters() {
+    if (this.OpponentRoster.length > 6) {
+      this.OpponentRoster.splice(6, this.OpponentRoster.length - 6); //id like this to be random
+    }
+
+    if (this.PlayerRoster.length > 6) {
+      this.PlayerRoster.splice(6, this.PlayerRoster.length - 6); //id like this to be random
+    }
+  }
+
+  OpponentChoosesCosmini() {
+    this.OpponentCosmini2Battle = this.OpponentRoster[Math.floor(Math.random() * 7)];
+  }
+
+  PlayerChoosesCosmini(Choice: number) {
+    this.PlayerCosmini2Battle = this.PlayerRoster[Choice];
+  }
+
+  ObtainBattleResult() {
+
+  }
   
   ngOnInit(): void {
   }
--- Cosminis/AngularCosminis/src/app/services/battle-service/battle.service.ts
+++ Cosminis/AngularCosminis/src/app/services/battle-service/battle.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Users } from 'src/app/Models/User';
+import { Friends } from 'src/app/Models/Friends';
 import { environment } from 'src/environments/environment';
+import { FriendsService } from '../Friends-api-service/friends.service';
+import { Cosminis } from 'src/app/Models/Cosminis';
 
 @Injectable({
   providedIn: 'root'
@@ -10,26 +13,32 @@ import { environment } from 'src/environments/environment';
   
 export class BattleService {
   
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private friend: FriendsService
+  ) { }
 
   apiUrl: string = environment.api + 'Battle/';
 
   //this generate a roster from a rando user
-  CreateRoster(): Observable<number[]> {
-    return this.http.get(this.apiUrl) as unknown as Observable<number[]>;
+  CreateRoster(): Observable<Cosminis[]> {
+    return this.http.get(this.apiUrl) as unknown as Observable<Cosminis[]>;
   }
+  
   //this generates a roster from a given userId
-  CreateRosterWithId(OpponentID: number): Observable<number[]> {
-    return this.http.get(this.apiUrl + OpponentID) as unknown as Observable<number[]>;
+  CreateRosterWithId(UserId: number): Observable<Cosminis[]> {
+    return this.http.get(this.apiUrl + UserId) as unknown as Observable<Cosminis[]>;
   }
+  
   //this will give who one!
   BattleResult(OpponentZero: number, OpponentOne: number): Observable<boolean> {
-    return this.http.get(this.apiUrl + "Result/" + OpponentOne + "/" + OpponentZero) as unknown as Observable<boolean>;
+    return this.http.get(this.apiUrl + "Result/" + OpponentZero + "/" + OpponentOne) as unknown as Observable<boolean>;
   }
+  
   //this obtains the difficult of the battle
   DifficultyScale(RosterOne: number[], RosterTwo: number[]): Observable<number> {
     return this.http.get(this.apiUrl + "Scalar"  + RosterOne + "/" + RosterTwo) as unknown  as Observable<number>;
   }
+  
   //the length of the match is determined by the roster with the smallest amount of cosminis
   BattleLength(RosterOne: number[], RosterTwo: number[]): number {
     if (RosterOne.length > RosterTwo.length) {
@@ -38,4 +47,40 @@ export class BattleService {
       return RosterOne.length;
     }
   }
+
+  GetRandomFriendsUserId(): number {
+    let stringUser: string = sessionStorage.getItem('currentUser') as string;
+    let currentUser: Users = JSON.parse(stringUser);
+    let FriendId: number = 0;
+    let friends: Friends[] = [];
+
+    this.friend.getAcceptedFriends(currentUser.username).subscribe((res) => friends = res);
+    
+    //gets a rando friends from user's friend list
+    let randoFriendinArr: number = Math.floor(Math.random() * friends.length);
+    if (friends[randoFriendinArr].userIdTo == currentUser.userId) {
+      FriendId = friends[randoFriendinArr].userIdFrom;
+    }
+    else {
+      FriendId = friends[randoFriendinArr].userIdTo;
+    }
+    return FriendId;
+  }
+
+  Payout(UserId: number, GoldBet: number, Difficulty: number, WinStreak: number): number {
+    let stringUser: string = sessionStorage.getItem('currentUser') as string;
+    let currentUser: Users = JSON.parse(stringUser);
+
+    if (WinStreak == 0 || Difficulty == -100) {
+      return 0;
+    }
+    
+    let NewGoldPayout: number = Math.pow(GoldBet * (1 + (.1)), .1 * WinStreak);
+    
+    NewGoldPayout = NewGoldPayout + (NewGoldPayout * (Difficulty / 100));
+
+    return NewGoldPayout;
+  }
+
+
 }
