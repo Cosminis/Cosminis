4f36ab4b16b95cdbba057435b3fe63f5617d0bad
krisholl <krisholl2022@gmail.com>
2022-09-02 14:20:10 -0400

    Merging from main

--- Cosminis/AngularCosminis/src/environments/environment.ts
+++ Cosminis/AngularCosminis/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = 
 {
   production: false, 
-  api: 'https://cosminis.azurewebsites.net/',
+  api: 'https://localhost:60150/',
   auth:
   {
     domain: "cosminis.us.auth0.com",
