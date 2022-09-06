d4281b38ad30718e1927cd56acb37bab00bd75da
krisholl <krisholl2022@gmail.com>
2022-09-06 14:20:08 -0400

    LotteryServiceTesting
    Signed-off-by: krisholl <krisholl2022@gmail.com>

---
+++ Cosminis/Tests/LotterySerivceTesting.cs
@@ -0,0 +1,61 @@
+﻿using Moq;
+using DataAccess.Entities;
+using CustomExceptions;
+using Services;
+using Xunit;
+using DataAccess;
+
+namespace Tests
+{
+    public class LotterySerivceTesting
+    {
+        /*
+         *      CanPlay Tests
+         */
+        [Fact]
+        public void CanPlayReturnsZeroWithImproperGemCount()
+        {
+
+        }
+        [Fact]
+        public void CanPlayReturnsValueWithProperGemCount()
+        {
+
+        }
+
+        /*
+         *      Winnings Test
+         */
+        [Fact]
+        public void WinningsReturnsListWithSpins()
+        {
+
+        }
+        [Fact]
+        public void WinningsReturnsNewListWithNoSpins()
+        {
+
+        }
+        /*
+         *      GiveRewards
+         */
+        [Fact]
+        public void GiveRewardsReturnsListOfWinningsWithSpins()
+        {
+
+        }
+        [Fact]
+        public void GiveRewardsReturnsNewListForNoSpins()
+        {
+
+        }
+        /*
+         *      Prize Tests
+         */
+        [Fact]
+        public void PrizeReturnsValueWithinRange()
+        {
+
+        }
+    }
+}
