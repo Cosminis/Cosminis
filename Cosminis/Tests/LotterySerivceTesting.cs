b6bf8fbff62547492363a6542a01e9adc382b160
krisholl <krisholl2022@gmail.com>
2022-09-06 14:38:15 -0400

    LotteryServiceTesting done
    Signed-off-by: krisholl <krisholl2022@gmail.com>

--- Cosminis/Tests/LotterySerivceTesting.cs
+++ Cosminis/Tests/LotterySerivceTesting.cs
@@ -15,12 +15,30 @@ namespace Tests
         [Fact]
         public void CanPlayReturnsZeroWithImproperGemCount()
         {
+            var moqRepo = new Mock<IResourceGen>();
+            LotteryService v = new(moqRepo.Object);
+            User user = new()
+            {
+                UserId = 1,
+                GemCount = 1
 
+            };
+            int spin = v.CanPlay(5, user);
+            Assert.Equal(0, spin);
         }
         [Fact]
         public void CanPlayReturnsValueWithProperGemCount()
         {
+            var moqRepo = new Mock<IResourceGen>();
+            LotteryService v = new(moqRepo.Object);
+            User user = new()
+            {
+                UserId = 1,
+                GemCount = 5
 
+            };
+            int spin = v.CanPlay(5, user);
+            Assert.Equal(1, spin);
         }
 
         /*
@@ -29,12 +47,20 @@ namespace Tests
         [Fact]
         public void WinningsReturnsListWithSpins()
         {
-
+            var moqRepo = new Mock<IResourceGen>();
+            LotteryService v = new(moqRepo.Object);
+            List<int> s = v.Winnings(1);
+            Assert.Equal(6, s.Count());
+            Assert.NotEmpty(s);
         }
         [Fact]
         public void WinningsReturnsNewListWithNoSpins()
         {
-
+            var moqRepo = new Mock<IResourceGen>();
+            LotteryService v = new(moqRepo.Object);
+            List<int> s = v.Winnings(0);
+            Assert.Equal(6, s.Count());
+            Assert.Equal(new int[6].ToList(), s);
         }
         /*
          *      GiveRewards
@@ -42,12 +68,32 @@ namespace Tests
         [Fact]
         public void GiveRewardsReturnsListOfWinningsWithSpins()
         {
+            var moqRepo = new Mock<IResourceGen>();
+            LotteryService v = new(moqRepo.Object);
+            User user = new()
+            {
+                UserId = 1,
+                GemCount = 5
 
+            };
+            List<int> ou = v.GiveRewards(1, user);
+            Assert.Equal(9, ou.Count());
+            Assert.NotEmpty(ou);
         }
         [Fact]
         public void GiveRewardsReturnsNewListForNoSpins()
         {
+            var moqRepo = new Mock<IResourceGen>();
+            LotteryService v = new(moqRepo.Object);
+            User user = new()
+            {
+                UserId = 1,
+                GemCount = 5
 
+            };
+            List<int> ou = v.GiveRewards(0, user);
+            Assert.Equal(9, ou.Count());
+            Assert.Equal(new int[9].ToList(), ou);
         }
         /*
          *      Prize Tests
@@ -55,6 +101,11 @@ namespace Tests
         [Fact]
         public void PrizeReturnsValueWithinRange()
         {
+            var moqRepo = new Mock<IResourceGen>();
+            LotteryService v = new(moqRepo.Object);
+            int rn = v.Prize(); 
+             
+            Assert.InRange(rn, 1, 100);
 
         }
     }
